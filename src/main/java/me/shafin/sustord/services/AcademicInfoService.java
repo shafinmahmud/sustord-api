8b4e478d8943232c0619cbe58862f0a9f3844ebf
Shafin Mahmud <shafin.mahmud@gmail.com>
2015-07-14 02:31:11 +0600

    Student_Batch table altered in database. so in the Entity.

--- src/main/java/me/shafin/sustord/services/AcademicInfoService.java
+++ src/main/java/me/shafin/sustord/services/AcademicInfoService.java
@@ -4,6 +4,8 @@ package me.shafin.sustord.services;
 
 import java.sql.SQLException;
 import me.shafin.sustord.models.AcademicProfile;
+import me.shafin.sustord.models.Department;
+import me.shafin.sustord.models.Program;
 import org.hibernate.HibernateException;
 
 /**
@@ -11,8 +13,8 @@ import org.hibernate.HibernateException;
  * @author SHAFIN
  */
 public class AcademicInfoService extends StudentIdentityService {
-
-    public AcademicInfoService(String registrationNo) throws HibernateException, NullPointerException, SQLException{
+    
+    public AcademicInfoService(String registrationNo) throws HibernateException, NullPointerException, SQLException {
         super(StudentIdentityService.forSingletonIdentityService(registrationNo));
     }
 
@@ -20,70 +22,78 @@ public class AcademicInfoService extends StudentIdentityService {
     public String getStudentSchoolName() {
         return studentInfo.getStudentBatchIdFk().getDegreeOfferedIdFk().getDeptIdFk().getSchoolIdFk().getSchoolName();
     }
-
+    
     public String getStudentDepartmentName() {
         return studentInfo.getStudentBatchIdFk().getDegreeOfferedIdFk().getDeptIdFk().getDeptName();
     }
-
+    
+    public Department getDepartment() {
+        String deptCode = this.studentInfo.getStudentBatchIdFk().getDegreeOfferedIdFk().getDeptIdFk().getDeptCode();
+        String deptName = this.getStudentDepartmentName();
+        String school = this.getStudentSchoolName();
+        return new Department(deptCode, deptName, school);
+    }
+    
     public String getStudentDegreeType() {
         String degreeType = studentInfo.getStudentBatchIdFk().getDegreeOfferedIdFk().getDegreeIdFk().getDegreeTypeName();
         return degreeType;
     }
-
+    
     public String getStudentDegreeCategory() {
         String degreeCategory = studentInfo.getStudentBatchIdFk().getDegreeOfferedIdFk().getDegreeIdFk().getDegreeCategory();
         return degreeCategory;
     }
-
+    
     public String getStudentProgramName() {
-        String programName = getStudentDegreeType()+ " (" + getStudentDegreeCategory() + ")";
+        String programName = getStudentDegreeType() + " (" + getStudentDegreeCategory() + ")";
         return programName;
+    }
+    
+    public Program getProgram() {
+        return new Program(this.getStudentDegreeType(), this.getStudentDegreeCategory());
     }
     
     public String getStudentAcademicSession() {
         return studentInfo.getStudentBatchIdFk().getSession();
     }
-
+    
     public int getTotalAcadmicSemester() {
         return studentInfo.getStudentBatchIdFk().getDegreeOfferedIdFk().getDegreeIdFk().getTotalSemester();
     }
     
-    public int getMaxAcademicSemester(){
+    public int getMaxAcademicSemester() {
         return studentInfo.getStudentBatchIdFk().getMaxSemester();
     }
     
-    public int getCurrentAcademicSemester(){
+    public int getCurrentAcademicSemester() {
         return 7; // need to call another service class to get computed this value;
     }
     
-    public int getTotalCourses(){
+    public int getTotalCourses() {
         return 72; //need to compute the value from syllabus service call
     }
     
-    public int getCoursesCompleted(){
+    public int getCoursesCompleted() {
         return 56; // need computation
     }
     
-    public double getTotalCredits(){
+    public double getTotalCredits() {
         return 160.0; //attention
     }
     
-    public double getCreditsCompleted(){
+    public double getCreditsCompleted() {
         return 102.5;//attention
     }
     
-    public double getCurrentCgpa(){
+    public double getCurrentCgpa() {
         return 3.60;// attention
     }
     
-    public AcademicProfile getAllAcademicInfo(){
+    public AcademicProfile getAllAcademicInfo() {
         AcademicProfile academic = new AcademicProfile(this.studentInfo.getRegistrationNo(),
-                                                                    this.studentInfo.getPersonalInfo().getName());
-        academic.setSchoolName(this.getStudentSchoolName());
-        academic.setDepartmentName(this.getStudentDepartmentName());
-        academic.setDegreeType(this.getStudentDegreeType());
-        academic.setDegreeCategory(this.getStudentDegreeCategory());
-        academic.setProgramName(this.getStudentProgramName());
+                this.studentInfo.getPersonalInfo().getName());
+        academic.setDepartment(this.getDepartment());
+        academic.setProgram(this.getProgram());
         academic.setMaxAcademicSemester(this.getMaxAcademicSemester());
         academic.setTotalAcademicSemester(this.getTotalAcadmicSemester());
         academic.setCurrentAcademicSemester(this.getCurrentAcademicSemester());
@@ -94,6 +104,6 @@ public class AcademicInfoService extends StudentIdentityService {
         academic.setCreditsCompleted(this.getCreditsCompleted());
         academic.setCurrentCgpa(this.getCurrentCgpa());
         
-        return academic; 
+        return academic;
     }
 }
